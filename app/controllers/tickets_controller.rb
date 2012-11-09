c91c37f03d3ea6bffcc112eae276e82b5c49530f
Frank Groeneveld <frank@ivaldi.nl>
2012-11-09 16:47:16 +0100

    Assign to user.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -5,5 +5,21 @@ class TicketsController < ApplicationController
 
   def index
     @tickets = Ticket.all
+    @users = User.all
+  end
+
+  def update
+    @ticket = Ticket.find(params[:id])
+
+    respond_to do |format|
+      if @ticket.update_attributes(params[:ticket])
+        format.html { redirect_to @ticket, notice: 'Ticket was successfully updated.' }
+        format.js { render notice: 'Ticket was succesfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @ticket.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
