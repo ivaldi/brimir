c60ffd61dae3ccbd3296d4180030b7c9f2044c4e
Frank Groeneveld <frank@ivaldi.nl>
2012-11-09 15:42:03 +0100

    Load tickets in index view

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,7 @@
 class Ticket < ActiveRecord::Base
-  attr_accessible :content, :from, :subject
+  attr_accessible :content, :from, :subject, :status_id
+
+  validates_presence_of :status_id
 
   belongs_to :status
 end
