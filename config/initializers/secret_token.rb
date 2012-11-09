6fd1a975da5a344e51f4d930e0c768ce34ef7d6a
Frank Groeneveld <frank@ivaldi.nl>
2012-11-09 14:12:47 +0100

    Initial Rails setup.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helpdesksysteem::Application.config.secret_token = 'c33f150e251cf5b03943d1be1ae6e4614ab893eb0c5d5e63f73b8b73ce40e3a3ab0e6995dee62af924f61e33f119ae8fc52c47db4c4dca295ffe021e72e4e372'
