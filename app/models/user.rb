8d3c3c16602aa31d7207770b694e777e141b77a9
Frank Groeneveld <frank@ivaldi.nl>
2012-11-09 14:44:06 +0100

    Initial users and login.

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable, :registerable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+end
