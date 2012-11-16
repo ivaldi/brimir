469373e04c347cb491eeadd7399b52d21a416ab9
Frank Groeneveld <frank@ivaldi.nl>
2012-11-16 14:52:04 +0100

    Handle multipart incoming e-mail correctly.

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -2,11 +2,34 @@ class TicketMailer < ActionMailer::Base
   default from: "from@example.com"
 
   def receive(email)
-    Ticket.create({
+    content = ''
+    if email.multipart?
+      if email.html_part
+        content = email.text_part.body.decoded
+      else 
+        content = email.html_part.body.decoded 
+      end
+    else
+      content = email.body.decoded
+    end
+
+    ticket = Ticket.create({
       from: email.from.join(', '),
       subject: email.subject,
-      content: email.body
+      content: content,
+      status_id: Status.where(name: 'New')
     })
+
+    if email.has_attachments?
+
+      email.attachments.each do |attachment|
+
+        ticket.attachments.create()
+
+      end
+
+    end
+
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121109144439) do
+ActiveRecord::Schema.define(:version => 20121116133200) do
+
+  create_table "attachments", :force => true do |t|
+    t.integer  "attachable_id"
+    t.string   "attachable_type"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
+
+  add_index "attachments", ["attachable_id"], :name => "index_attachments_on_attachable_id"
 
   create_table "statuses", :force => true do |t|
     t.string   "name"
