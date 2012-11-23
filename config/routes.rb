07873f7032ef68430894879ffb4dc127d14422ec
Frank Groeneveld <frank@ivaldi.nl>
2012-11-23 14:50:05 +0100

    Clean unused/generated resources.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Helpdesksysteem::Application.routes.draw do
 
-  resources :replies, only: [:create]
+  resources :replies, only: [ :create ]
 
   devise_for :users
 
   resources :users
 
-  resources :tickets, only: [:index, :show, :update]
+  resources :tickets, only: [ :index, :show, :update ]
 
   root :to => 'tickets#index'
 
