2e8ebe0c8f2e6119b8b80d9930aab69f6d6303b2
Frank Groeneveld <frank@ivaldi.nl>
2012-12-07 13:58:58 +0100

    Don't initialize databases etc to precompile assets.

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module Brimir
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
   end
 end
