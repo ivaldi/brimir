1cb2d22ee595c886f89621a0ffe152d6cf11430f
Frank Groeneveld <frank@ivaldi.nl>
2012-12-14 15:34:54 +0100

    Use sendmail by default in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,8 @@ Brimir::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable threaded mode
   # config.threadsafe!
