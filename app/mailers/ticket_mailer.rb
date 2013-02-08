b341c98d21190eb3a408038c4d75638dbdd73150
Frank Groeneveld <frank@ivaldi.nl>
2013-02-08 14:50:23 +0100

    Remove default mail address.

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -15,7 +15,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 class TicketMailer < ActionMailer::Base
-  default from: "from@example.com"
 
   def reply(reply)
     @reply = reply
