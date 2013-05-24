0d9d71ca14c9c80007732ecbeec69aab1968caa3
Frank Groeneveld <frank@ivaldi.nl>
2013-05-24 15:15:44 +0200

    Test for index view of tickets.

---
+++ test/functional/tickets_controller_test.rb
@@ -0,0 +1,34 @@
+# Brimir is a helpdesk system to handle email support requests.
+# Copyright (C) 2012 Ivaldi http://ivaldi.nl
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'test_helper'
+
+class TicketsControllerTest < ActionController::TestCase
+
+  setup do
+
+    @ticket = tickets(:problem)
+
+    sign_in users(:alice)
+  end
+
+  test 'should get index' do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:tickets)
+  end
+
+end
