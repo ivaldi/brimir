2e98006d10691e1d4b7d4058aa453231ce145b35
Frank Groeneveld <frank@ivaldi.nl>
2013-06-28 14:35:03 +0200

    Fix seed file to throw exceptions on errors.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,23 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-frank = User.create({ email: 'frank@xxxx.com', password: 'testtest', password_confirmation: 'testtest', agent: true })
-sem = User.create({ email: 'sem@xxxx.com', password: 'testtest', password_confirmation: 'testtest', agent: true })
+frank = User.new({ email: 'frank@xxxx.com', password: 'testtest', password_confirmation: 'testtest' })
+frank.agent = true
+frank.save!
 
-Status.create({ name: 'Deleted' })
-status_closed = Status.create({ name: 'Closed' })
-status_open = Status.create({ name: 'Open', default: true })
+sem = User.create({ email: 'sem@xxxx.com', password: 'testtest', password_confirmation: 'testtest' })
+sem.agent = true
+sem.save!
+
+Status.create!({ name: 'Deleted' })
+status_closed = Status.create!({ name: 'Closed' })
+status_open = Status.create!({ name: 'Open', default: true })
 
 password_length = 12
 password = Devise.friendly_token.first(password_length)
-owner = User.create(email: 'test@xxxx.com', password: password, password_confirmation: password)
+owner = User.create!(email: 'test@xxxx.com', password: password, password_confirmation: password)
 
-Ticket.create([
+Ticket.create!([
   { status_id: status_open.id, user_id: owner.id, subject: 'I have some problems', content: '<pre>I have problems with my computer. Please help</pre>', assignee_id: frank.id, message_id: '1@xxxx.com' },
   { status_id: status_closed.id, user_id: owner.id, subject: 'I had some problems', content: '<pre>I have problems with my computer. Please help</pre>', message_id: '2@xxxx.com' },
   { status_id: status_open.id, user_id: owner.id, subject: 'I had some problems', content: '<pre>I have problems with my computer. Please help</pre>', assignee_id: sem.id, message_id: '3@xxxx.com' }
