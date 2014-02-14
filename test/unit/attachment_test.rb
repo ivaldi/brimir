87ea9237d0281fff2ea8e770bac134374ab8feb1
Frank Groeneveld <frank@ivaldi.nl>
2014-02-14 16:34:29 +0100

    Update copyright notice

--- test/unit/attachment_test.rb
+++ test/unit/attachment_test.rb
@@ -1,5 +1,5 @@
 # Brimir is a helpdesk system that can be used to handle email support requests.
-# Copyright (C) 2012 Ivaldi http://ivaldi.nl
+# Copyright (C) 2012-2014 Ivaldi http://ivaldi.nl
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
