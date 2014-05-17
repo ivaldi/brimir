5aa137618aae35b29959ba08e019e6d129573629
James Boutcher <jim@boutcher.com>
2014-05-17 06:40:55 -0500

    fixed migration that caused initial mysql migration to fail, and then defaulted the project to sqlite

--- Gemfile
+++ Gemfile
@@ -42,8 +42,9 @@ group :test do
 end
 
 group :production do
-  # PostgreSQL for production
-  gem 'pg'
+  # if either of these please you ...
+  # gem 'pg'
+  # gem 'mysql2'
 end
 
 # Authentication
--- db/migrate/20130927133039_add_content_type_to_replies.rb
+++ db/migrate/20130927133039_add_content_type_to_replies.rb
@@ -1,6 +1,6 @@
 class AddContentTypeToReplies < ActiveRecord::Migration
   def change
-    add_column :replies, :content_type, :text
-    add_column :tickets, :content_type, :text
+    add_column :replies, :content_type, :string, default: 'markdown'
+    add_column :tickets, :content_type, :string, default: 'html'
   end
 end
