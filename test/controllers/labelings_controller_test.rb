f35a54de8bb6efb88ea00ba1b6238eb3a8f5102e
Frank Groeneveld <frank@ivaldi.nl>
2014-07-25 17:05:10 +0200

    Add tests for creating labelings

--- test/controllers/labelings_controller_test.rb
+++ test/controllers/labelings_controller_test.rb
@@ -24,6 +24,22 @@ class LabelingsControllerTest < ActionController::TestCase
     sign_in users(:alice)
   end
 
+  test 'should create labeling' do
+
+    assert_difference 'Labeling.count' do
+
+      post :create, format: :js, labeling: {
+        labelable_id: tickets(:problem).id,
+        labelable_type: 'Ticket',
+        label: {
+          name: 'Hello'
+        }
+      }
+
+      assert_response :success
+    end
+  end
+
   test 'should remove labeling' do
     assert_difference 'Labeling.count', -1 do
       delete :destroy, id: @labeling, format: :js
