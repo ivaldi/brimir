a23fc3bc053cbc33d3c5a9ab15c5efa3937ce09e
Frank Groeneveld <frank@ivaldi.nl>
2014-07-25 16:01:59 +0200

    Add label show and remove for tickets

---
+++ app/controllers/labelings_controller.rb
@@ -0,0 +1,28 @@
+# Brimir is a helpdesk system to handle email support requests.
+# Copyright (C) 2012-2014 Ivaldi http://ivaldi.nl
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+class LabelingsController < ApplicationController
+
+  load_and_authorize_resource :labeling
+
+  def destroy
+    @labelings = Labeling.find(params[:id])
+
+    @labelings.destroy
+
+    respond_to :js
+  end
+end
