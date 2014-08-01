940cb07df9801dd90cba19dee6a10be5b4bc4c60
Frank Groeneveld <frank@ivaldi.nl>
2014-08-01 15:33:19 +0200

    Cleanup

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -29,7 +29,6 @@ class Ability
       # customers can view their own tickets, its replies and attachments
       can [:read, :create, :new], Ticket, user_id: user.id
       can [:new, :create, :read], Reply, ticket: { user_id: user.id }
-      can [:create], Reply, ticket: nil # preview reply
       can :read, Attachment, attachable_type: 'Ticket', attachable: { user_id: user.id }
       can :read, Attachment, attachable_type: 'Reply', attachable: { ticket: { user_id: user.id } }
 
