bd8d410d94b7c91a928812b24d5c4244751acb62
Frank Groeneveld <frank@frankgroeneveld.nl>
2014-08-08 21:02:45 +0200

    Add users controller test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -18,51 +18,50 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
 
+  setup do
+    @alice = users(:alice)
+    @bob = users(:bob)
+  end
+
   test 'should get index' do
-    alice = users(:alice)
-    sign_in alice
+    sign_in @alice
 
     get :index
     assert_response :success
   end
 
   test 'should not get index' do
-    bob = users(:bob)
-    sign_in bob
+    sign_in @bob
 
     get :index
     assert_response :unauthorized
   end
 
   test 'should get edit' do
-    alice = users(:alice)
-    sign_in alice
+    sign_in @alice
 
-    get :edit, id: alice.id
+    get :edit, id: @alice.id
     assert_response :success
   end
 
   test 'should modify signature' do
-    alice = users(:alice)
-    sign_in alice
+    sign_in @alice
 
-    put :update, id: alice.id, user: { signature: 'Alice' }
+    put :update, id: @alice.id, user: { signature: 'Alice' }
     assert_equal 'Alice', assigns(:user).signature
     assert_redirected_to users_url
   end
 
   test 'customer may not become agent' do
-    bob = users(:bob)
-    sign_in bob
+    sign_in @bob
 
     assert_no_difference 'User.agents.count' do
-      put :update, id: bob.id, user: { agent: true, signature: 'Bob' }
+      put :update, id: @bob.id, user: { agent: true, signature: 'Bob' }
     end
   end
 
   test 'customer may not create agent' do
-    bob = users(:bob)
-    sign_in bob
+    sign_in @bob
 
     assert_no_difference 'User.agents.count' do
       post :create, user: {
@@ -75,5 +74,44 @@ class UsersControllerTest < ActionController::TestCase
     end
 
     assert_response :unauthorized
+  end
+
+  test 'should update user' do
+    sign_in @alice    
+
+    assert_difference 'Labeling.count' do
+      patch :update, id: @bob.id, user: {
+        email: 'test@test.test',
+        label_ids: [labels(:bug).id]
+      }
+    end
+    
+    @bob.reload
+
+    assert_equal 'test@test.test', @bob.email
+    assert_equal 1, @bob.labels.count
+    assert_equal labels(:bug), @bob.labels.first
+
+    assert_redirected_to users_url
+  end
+
+  test 'should not update user' do
+    sign_in @bob
+
+    assert_no_difference 'Labeling.count' do
+      patch :update, id: @bob.id, user: {
+        email: 'test@test.test',
+        label_ids: [labels(:bug).id],
+        password: 'testtest',
+        password_confirmation: 'testtest',
+      }
+    end
+    
+    @bob.reload
+
+    refute_equal 'test@test.test', @bob.email
+    assert_equal 0, @bob.labels.count
+
+    assert_redirected_to tickets_url
   end
 end
