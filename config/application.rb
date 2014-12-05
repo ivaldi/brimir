d14e8cdc63ccc6207f0c54a17c0b6d3d67de6ce2
Frank Groeneveld <frank@ivaldi.nl>
2014-12-05 16:59:50 +0100

    Make buttons translatable

--- config/application.rb
+++ config/application.rb
@@ -31,9 +31,10 @@ module Brimir
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
+    config.i18n.fallbacks = [:en]
 
     # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
+    config.encoding = 'utf-8'
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
