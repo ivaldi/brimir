e289bd05d3ddbe076c04e2cdaadee72d48ff572b
Frank Groeneveld <frank@ivaldi.nl>
2014-12-19 15:26:30 +0100

    Add prepartions for user configurable outgoing email addresses

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 class NotificationMailer < ActionMailer::Base
+  default from: EmailAddress.default_email
 
   add_template_helper HtmlTextHelper
 
---
+++ app/models/email_address.rb
@@ -0,0 +1,34 @@
+# Brimir is a helpdesk system that can be used to handle email support requests.
+# Copyright (C) 2012-2014 Ivaldi http://ivaldi.nl
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+class EmailAddress < ActiveRecord::Base
+
+  def self.default_email
+
+    if !EmailAddress.where(default: true).first.nil?
+      return EmailAddress.where(default: true).first.email
+
+    elsif ActionMailer::Base.default[:from].present?
+      ActionMailer::Base.default[:from]
+
+    elsif Rails.configuration.action_mailer.default_options.present?
+      Rails.configuration.action_mailer.default_options[:from]
+
+    end
+
+  end
+
+end
