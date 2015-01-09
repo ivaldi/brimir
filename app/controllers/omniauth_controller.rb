df4906ed75c584c014cc50758331d1f334c38856
Frank Groeneveld <frank@ivaldi.nl>
2015-01-09 14:37:11 +0100

    Use devise "approved" sign_in_and_redirect to also support other urls than root_url.

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -21,10 +21,8 @@ class OmniauthController < Devise::OmniauthCallbacksController
       end
     else
       if @identity.user.present?
-        # the identity we found had a user associated with it so let's
-        # just log them in here
-        sign_in @identity.user
-        redirect_to root_url, notice: I18n.translate(:signed_in_with_omniauth)
+        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+        sign_in_and_redirect @identity.user, event: :authentication
       else
         # no user associated with the identity so we reject this attemp
         redirect_to new_user_session_path, alert: I18n.translate(:not_linked_account_cant_login)
