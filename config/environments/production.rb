0b707a48ce79eec22b9cb659627fc5e4bd2d9794
Frank Groeneveld <frank@ivaldi.nl>
2015-01-30 08:45:37 +0100

    Use sendmail by default again.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,8 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :sendmail
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
