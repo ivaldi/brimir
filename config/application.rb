396a92739d8f866e9e1ca4f64dc68907c3b732f9
Andrei Lisnic <andrei.lisnic@gmail.com>
2015-03-05 19:44:32 +0200

    Put auth strategy in config

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,9 @@ module Brimir
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+
+    # Change this to :ldap_authenticatable to use ldap
+    config.devise_authentication_strategy = :database_authenticatable
     config.i18n.default_locale = :en
     config.i18n.fallbacks = [:en]
 
