a42db0e8a957e592dce76f093630111ba203c462
Andrei Lisnic <andrei.lisnic@gmail.com>
2015-03-05 12:36:04 +0200

    User Ruby 1.9.3 hash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@
 
 class ApplicationController < ActionController::Base
   rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
-    render :text => exception, :status => 500
+    render text: exception, status: 500
   end
   protect_from_forgery
 
