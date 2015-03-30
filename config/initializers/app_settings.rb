531405b3e18eff3805c8c1748d81f895de511c24
modsaid <mahmoud@modsaid.com>
2015-03-30 18:58:52 +0200

    handling coding guidelines notes

--- config/initializers/app_settings.rb
+++ config/initializers/app_settings.rb
@@ -3,20 +3,15 @@ class AppSettings
     config_file = File.join(Rails.root, "config", "settings.yml")
     
     if File.exists?(config_file)
-      all_config = YAML.load(File.read(config_file)) 
-      config = {}
-      ["default", Rails.env].each do |group|
-        config.merge!(all_config[group]) unless all_config[group].blank?
-      end
-
-      config.keys.reject{|k| k=="<<"}.each do |key|
+      config = YAML.load(File.read(config_file))[Rails.env]
+      config.keys.reject { |k| k == "<<" }.each do |key|
         cattr_accessor key
         send("#{key}=", config[key])
       end
     end
   end
 
-  def self.method_missing(*args)
+  def self.method_missing(*)
     nil
   end
 end
