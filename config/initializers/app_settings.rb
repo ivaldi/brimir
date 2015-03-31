c993ed38e734bb8641c4e04b252666cef4eeb392
modsaid <eng.mahmoud.said@gmail.com>
2015-03-31 15:35:14 +0200

    Reducing line size

--- config/initializers/app_settings.rb
+++ config/initializers/app_settings.rb
@@ -1,6 +1,7 @@
-# AppSettings is used an a configuration wrapper. It loads configuration from config/settings.yml and
-# create class getters for each of the attributes.
-# The values can be retrieved in the code via   AppSettings.config_name (example: AppSettings.ignore_user_agent_locale)
+# AppSettings is used an a configuration wrapper. It loads configuration from 
+# config/settings.yml and creates class getters for each of the attributes.
+# The values can be retrieved in the code via   AppSettings.config_name 
+# (example: AppSettings.ignore_user_agent_locale)
 # if a method is called but it is not in the settings.yml, it will just return nil 
 
 class AppSettings
