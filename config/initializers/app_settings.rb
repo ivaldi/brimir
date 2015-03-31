340e0cb818cccb5738a3aa0eedabd3ab6ab958f2
modsaid <eng.mahmoud.said@gmail.com>
2015-03-31 15:36:05 +0200

    removing training spaces

--- config/initializers/app_settings.rb
+++ config/initializers/app_settings.rb
@@ -1,9 +1,8 @@
-# AppSettings is used an a configuration wrapper. It loads configuration from 
+# AppSettings is used an a configuration wrapper. It loads configuration from
 # config/settings.yml and creates class getters for each of the attributes.
-# The values can be retrieved in the code via   AppSettings.config_name 
+# The values can be retrieved in the code via   AppSettings.config_name
 # (example: AppSettings.ignore_user_agent_locale)
-# if a method is called but it is not in the settings.yml, it will just return nil 
-
+# if a method is called but it is not in the settings.yml, it will just return nil
 class AppSettings
   def self.load
     config_file = File.join(Rails.root, "config", "settings.yml")
