62fa6ea08ebdfdab81f318ad5a54da643a0cd156
vartana <vartana@gmail.com>
2015-07-16 22:42:20 -0700

    Update boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -2,11 +2,14 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 
+require 'rubygems'
 require 'rails/commands/server'
+
 module Rails
   class Server
+    alias :default_options_alias :default_options
     def default_options
-      super.merge(Host:  '0.0.0.0', Port: 3000)
+      default_options_alias.merge!(:Host => '0.0.0.0')
     end
   end
 end
