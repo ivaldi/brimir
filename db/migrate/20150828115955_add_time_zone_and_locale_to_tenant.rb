7ac07b772ddf7fd7c904292c420949b82dd479c7
Frank Groeneveld <frank@ivaldi.nl>
2015-08-28 14:24:43 +0200

    Fix migration to work with multiple schema

--- db/migrate/20150828115955_add_time_zone_and_locale_to_tenant.rb
+++ db/migrate/20150828115955_add_time_zone_and_locale_to_tenant.rb
@@ -1,8 +1,20 @@
 class AddTimeZoneAndLocaleToTenant < ActiveRecord::Migration
   def change
-    add_column :tenants, :default_time_zone, :string, default: 'Amsterdam'
-    add_column :tenants, :ignore_user_agent_locale, :boolean, default: false, null: false
-    add_column :tenants, :default_locale, :string, default: 'en'
+    if Tenant.postgresql?
+      old = Tenant.connection.schema_search_path
+      Tenant.connection.schema_search_path = 'public'
+    end
+
+    unless column_exists? :tenants, :default_time_zone
+      add_column :tenants, :default_time_zone, :string, default: 'Amsterdam'
+      add_column :tenants, :ignore_user_agent_locale, :boolean, default: false, null: false
+      add_column :tenants, :default_locale, :string, default: 'en'
+    end
+
+    if Tenant.postgresql?
+      Tenant.connection.schema_search_path = old
+    end
+
     change_column_default :users, :time_zone, nil
   end
 end
