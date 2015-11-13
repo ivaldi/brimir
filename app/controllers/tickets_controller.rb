aa5cc63b00c2dd2469117feff96b9f4e93eae578
Bas Schoenmakers <bas@ivaldi.nl>
2015-11-13 16:39:44 +0100

    reorder form internal notes

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -41,7 +41,7 @@ class TicketsController < ApplicationController
       @reply = draft
     else
       @reply = @ticket.replies.new(user: current_user)
-      @reply.reply_to = @replies.last || @ticket
+      @reply.reply_to = @replies.select{ |r| !r.internal? }.last || @ticket
       @reply.set_default_notifications!
     end
 
