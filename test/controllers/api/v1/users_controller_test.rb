2e3887fbddfc612085e1c34ea4aa25e7542087e4
Sylvain <sylvain@bellino.fr>
2016-01-14 14:44:22 +0100

    Added users to API :
    Create users
    Find user by mail
    Tickets by user

---
+++ test/controllers/api/v1/users_controller_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class Api::V1::UsersControllerTest < ActionController::TestCase
+
+  setup do
+    @ticket = tickets(:problem)
+    @alice = users(:alice)
+    @bob = users(:bob)
+    @charlie = users(:charlie)
+  end
+
+  test 'should find a User' do
+    get :show, auth_token: users(:alice).authentication_token, email: users(:alice).email, :format => :json
+    assert_response :success
+  end
+
+  test 'should create a User ' do
+    sign_in users(:alice)
+    assert_difference 'User.count', 1 do
+      post :create, auth_token: users(:alice).authentication_token, user: {
+        email: 'newuser@new.com'}, 
+        format: :json
+    end
+    assert_response :success
+  end
+
+end
