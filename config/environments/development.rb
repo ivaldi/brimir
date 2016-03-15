f77e796dc967c49303c992143600d9ceaf9dd95d
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-03-15 18:45:53 +0100

    Adding letter_opener gem to open emails in the browser in development.
    https://github.com/ryanb/letter_opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,10 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  # Open emails in the browser.
+  # https://github.com/ryanb/letter_opener
+  config.action_mailer.delivery_method = :letter_opener
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
