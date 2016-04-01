5d0efd288b9fc95e05fe7bf2331ff62a46b7fc17
Frank Groeneveld <frank@ivaldi.nl>
2016-04-01 13:42:29 +0200

    Remove whitespace

--- app/models/merged_ticket.rb
+++ app/models/merged_ticket.rb
@@ -11,17 +11,17 @@
 #     merged_ticket = MergedTicket.from separate_tickets, current_user: current_user
 #
 class MergedTicket < Ticket
-  
+
   def initialize(separate_tickets, options = {})
     @original_tickets = separate_tickets
     @current_user = options[:current_user]
     return self
   end
-  
+
   def self.from(separate_tickets, options = {})
     self.new(separate_tickets, options).merge
   end
-  
+
   def merge
     oldest_original_ticket.replies << copies_of_the_replies_of_the_younger_tickets
     oldest_original_ticket.replies << younger_tickets.collect { |ticket| ticket.to_reply }
@@ -29,36 +29,36 @@ class MergedTicket < Ticket
     younger_tickets.each { |ticket| ticket.status = :merged; ticket.save }
     return oldest_original_ticket
   end
-  
+
   def current_user
     @current_user
   end
-  
+
   def original_tickets
     @original_tickets || raise('not properly initialized. @original_tickets are missing.')
   end
-  
+
   def oldest_original_ticket
     @oldest_original_ticket ||= original_tickets.sort_by { |ticket| ticket.created_at }.first
   end
-  
+
   def younger_tickets
     original_tickets.select { |ticket| ticket.id != oldest_original_ticket.id }
   end
-  
+
   def replies_of_the_younger_tickets
     younger_tickets.collect { |ticket| ticket.replies }.flatten
   end
-  
+
   def copies_of_the_replies_of_the_younger_tickets
-    replies_of_the_younger_tickets.collect do |reply| 
-      reply_copy = reply.dup 
+    replies_of_the_younger_tickets.collect do |reply|
+      reply_copy = reply.dup
       reply_copy.created_at = reply.created_at
       reply_copy.updated_at = reply.updated_at
       reply_copy.attachments << reply.attachments.collect { |attachment| attachment.dup }
       reply
     end
   end
-  
+
 end
 
