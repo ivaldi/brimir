4dfb7c19795274ef38dc9abbd139b8a644df3153
Frank Groeneveld <frank@ivaldi.nl>
2016-06-10 16:02:42 +0200

    Use correct class

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -45,7 +45,7 @@ class Tenant < ActiveRecord::Base
     end
 
     ActionMailer::Base.default_url_options = { host: "#{domain}" }
-    Rails.configuration.devise.mailer_sender = self.default_email
+    Rails.configuration.devise.mailer_sender = EmailAddress.default_email
 
     Paperclip.interpolates :domain do |attachment, style|
       # no schema based tenants, so no subdir
