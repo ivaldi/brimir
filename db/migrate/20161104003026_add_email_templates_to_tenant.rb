10c5e1681a5d42795c5c519e1209ecd362145718
Frank Groeneveld <frank@ivaldi.nl>
2016-11-11 14:03:57 +0100

    Prevent tenant error

--- db/migrate/20161104003026_add_email_templates_to_tenant.rb
+++ db/migrate/20161104003026_add_email_templates_to_tenant.rb
@@ -1,5 +1,7 @@
 class AddEmailTemplatesToTenant < ActiveRecord::Migration
   def change
-    add_reference :tenants, :email_template, index: true, foreign_key: true
+    unless column_exists? :tenants, :email_template_id
+      add_reference :tenants, :email_template, index: true, foreign_key: true
+    end
   end
 end
