9159dece1c15de31910645149d224164a214a977
Frank Groeneveld <frank@ivaldi.nl>
2016-11-25 15:07:37 +0100

    Cleanup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  # The page to create a ticket is open to the world by default?
-  config.ticket_creation_is_open_to_the_world_by_default = true
 end
