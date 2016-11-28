b58760bb964fb23ae822c0786d5d31be89d2978a
Mickael Kerjean <mickael.kerjean@gmail.com>
2016-11-28 01:04:56 +0100

    q Fix (#313): requested change on migration file

--- db/migrate/20161125125817_add_stylesheet_to_tenant.rb
+++ db/migrate/20161125125817_add_stylesheet_to_tenant.rb
@@ -1,5 +1,7 @@
 class AddStylesheetToTenant < ActiveRecord::Migration
   def change
-    add_column :tenants, :stylesheet_url, :string, default: Rails.application.config.tenant_default_stylesheet_url
+    unless column_exists? :tenants, :stylesheet_url
+      add_column :tenants, :stylesheet_url, :string, default: Rails.application.config.tenant_default_stylesheet_url
+    end
   end
 end
