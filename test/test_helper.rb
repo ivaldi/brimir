d3562ba9e029269050b4f33eb93cf1a198c96180
git-jls <mail@sjorssparreboom.nl>
2016-12-16 20:20:33 +0100

    Updated CHANGELOG.md

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -26,6 +26,7 @@ class ActiveSupport::TestCase
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
+  fixtures :schedules
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
