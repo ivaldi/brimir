1af32895420b4f3a57b36cc9af5a141386430c38
git-jls <mail@sjorssparreboom.nl>
2016-12-28 16:30:37 +0100

    Upcase for enviroment variables

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -26,7 +26,6 @@ class ActiveSupport::TestCase
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
-  fixtures :schedules
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
